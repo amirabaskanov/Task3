a7714b5649adc2645b8371e122fd8c3df53d70b9
Amir A <absknv@gmail.com>
2019-06-13 11:48:49 +0600

    Task3

---
+++ task3.py
@@ -0,0 +1,12 @@
+def is_odd(x): return x % 2 == 1
+
+
+def test_is_odd():
+  assert is_odd(2) == False
+  assert is_odd(3) == True
+  assert is_odd(8) == False
+  assert is_odd(100) == False
+  assert is_odd(101) == True
+  print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+test_is_odd()
